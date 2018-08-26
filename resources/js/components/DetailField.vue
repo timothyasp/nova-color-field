4bd0056ef4fa3b6f3351193d289e791c97e46848
Jorge Villafuerte <jorge@avantpage.com>
2018-08-26 17:42:37 +0200

    added color preview on detail view

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,5 +1,12 @@
 <template>
-    <panel-item :field="field" />
+    <panel-item :field="field">
+        <template slot="value">
+            <div class="inline-flex items-center">
+        	    <div class="rounded mr-2" v-bind:style="{ backgroundColor: field.value, width: '20px', height: '20px' }"></div>
+        	    <div>{{ field.value }}</div>
+    	    </div>	
+        </template>
+    </panel-item>
 </template>
 
 <script>
